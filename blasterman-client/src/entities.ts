1cd9b97151388c29ebd853a8566b4e1139f43eb4
Biffyclyro <joao-senna1@hotmail.com>
2021-05-18 18:12:59 -0300

    criação das animações do personagem

--- blasterman-client/src/entities.ts
+++ blasterman-client/src/entities.ts
@@ -3,9 +3,9 @@ import {Room} from './core/room';
 
 export enum Direction {
   Up = 1,
-  Down,
-  Right,
-  Left,
+    Down,
+    Right,
+    Left,
 }
 
 export class Player extends Sprite{
@@ -15,21 +15,48 @@ export class Player extends Sprite{
   scene: Room;
 
   constructor(scene: Room, 
-              x: number, 
-              y: number) {
+    x: number, 
+    y: number) {
     super(scene, x, y, key);
-    
+
     this.scene.anims.create({
-      key: 'side-walk';
-      frames:[{key: this.skin,
-               frame: },
-              {},
-              {}], 
+      key: 'side-walk',
+      frame: this.scene.anims.generateFrameNumbers({
+        this.skin, {start: 0, end: 3}
+      }),
       framRate: 10,
       repeat: 0
     });
+
+    this.scene.anims.create({
+      key: 'up-walk',
+      frame: this.scene.anims.generateFrameNumbers({
+        this.skin, {start: 4, end: 7}
+      }),
+      framRate: 10,
+      repeat: 0
+    });
+
+    this.scene.anims.create({
+      key: 'down-walk',
+      frame: this.scene.anims.generateFrameNumbers({
+        this.skin, {start: 8, end: 11}
+      }),
+      framRate: 10,
+      repeat: 0
+    });
+
+    this.scene.anims.create({
+      key: 'dead',
+      frame: this.scene.anims.generateFrameNumbers({
+        this.skin, {start: 12, end: 17}
+      }),
+      framRate: 10,
+      repeat: 0
+    });
+
   }
-  
+
 
 
 
